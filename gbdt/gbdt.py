cb9356dcf3b291fc6ea0ecbedb4c0b87e92c1a45
takeru.abe <takeru.abe@gaiax.com>
2022-01-14 16:12:30 +0900

    gbrt max_depth = 1

--- gbdt/gbdt.py
+++ gbdt/gbdt.py
@@ -2,12 +2,14 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 
+
 def execute():
     cancer = load_breast_cancer()
     X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
 
-    gbrt = GradientBoostingClassifier(random_state=0)
+    gbrt = GradientBoostingClassifier(random_state=0,max_depth=1)
     gbrt.fit(X_train, y_train)
 
     print("Accuracy of training set: {:.3f}".format(gbrt.score(X_train, y_train)))
     print("Accuracy of test set: {:.3f}".format(gbrt.score(X_test, y_test)))
+
