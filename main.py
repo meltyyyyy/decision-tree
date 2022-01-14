e4e384afeeb78cb4d7133e310600fb5f8133bd1e
takeru.abe <takeru.abe@gaiax.com>
2022-01-14 16:10:15 +0900

    gradient boosting decision tree

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from decision_tree import decision_tree
 from dtr import dtr
+from gbdt import gbdt
 from random_forest import random_forest
 
-decision_tree.execute()
-dtr.execute()
-random_forest.execute()
+# decision_tree.execute()
+# dtr.execute()
+# random_forest.execute()
+gbdt.execute()
